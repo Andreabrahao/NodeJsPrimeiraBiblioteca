f4eb26e9cbd4a9b8e232b13992fc32dca33339dc
André Abrahão <abrahaoam@gmail.com>
2023-07-21 03:01:48 -0300

    NodeJs com JavaScript

---
+++ notas.js
@@ -0,0 +1,23 @@
+// promises com then
+
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = "utf-8";
+//     fs.promises
+//         .readFile(caminhoDoArquivo, encoding)
+//         .then((texto) => console.log(chalk.green(texto)))
+//         .catch(trataErro);
+// }
+
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = "utf-8";
+//     fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+//         if (erro) {
+//             trataErro(erro);
+//         }
+//         console.log(chalk.green(texto));
+//     });
+// }
+
+// \[[^[\]]*?\]
+// \(https?:\/\/[^\s?#.].[^\s]*\)
+// \[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)
--- src/index.js
+++ src/index.js
@@ -27,27 +27,3 @@ async function pegaArquivo(caminhoDoArquivo) {
 }
 
 export default pegaArquivo;
-
-// promises com then
-
-// function pegaArquivo(caminhoDoArquivo) {
-//     const encoding = "utf-8";
-//     fs.promises
-//         .readFile(caminhoDoArquivo, encoding)
-//         .then((texto) => console.log(chalk.green(texto)))
-//         .catch(trataErro);
-// }
-
-// function pegaArquivo(caminhoDoArquivo) {
-//     const encoding = "utf-8";
-//     fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-//         if (erro) {
-//             trataErro(erro);
-//         }
-//         console.log(chalk.green(texto));
-//     });
-// }
-
-// \[[^[\]]*?\]
-// \(https?:\/\/[^\s?#.].[^\s]*\)
-// \[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)
