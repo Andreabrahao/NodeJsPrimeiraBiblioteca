76d3bec2601a262f55988383c4b6b214cf311433
André Abrahão <abrahaoam@gmail.com>
2023-07-25 08:34:12 -0300

    NodeJs co  JavaScript

--- notas.js
+++ notas.js
@@ -21,3 +21,7 @@
 // \[[^[\]]*?\]
 // \(https?:\/\/[^\s?#.].[^\s]*\)
 // \[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)
+
+// ENOTFOUND
+// Error: not found - Retorna quando o Node.js tenta estabelecer uma conexão com um servidor e a tentativa falha no DNS lookup; ou seja, ou o host não existe ou há algum erro no endereço fornecido, que não consegue ser resolvido pelo DNS.
+// Algumas das soluções possíveis: verifique se a URL fornecida realmente existe e se a string com a URL está sendo fornecida da forma correta para a função ou método que fará a conexão.
